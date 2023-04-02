259bb60de7ad91d2f593b881e94b59d24971adb9
gin <gin-18@qq.com>
2023-04-02 21:32:14 +0800

    change theme to catppuccin-frappe theme

--- config.def.h
+++ config.def.h
@@ -11,9 +11,9 @@ static const char *fonts[] = {
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	[SchemeNorm] = { "#d8dee9", "#3b4252" },
-	[SchemeSel] = { "#d8dee9", "#4c566a" },
-	[SchemeOut] = { "#000000", "#00ffff" },
+	[SchemeNorm] = { "#c6d0f5", "#303446" },
+	[SchemeSel] = { "#303446", "#99d1db" },
+	[SchemeOut] = { "#000000", "#99d1db" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 0;
